4db5791b491b6662a024da6e4f373859680e0b57
C. Annable <christopher.annable@gmail.com>
2021-01-07 15:02:25 -0500

    Initial project import.

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM centos:8
+
+ARG NOMAD_VERSION
+ARG NOMAD_VERSION=1.0.1
+
+ENV NAME nomad
+
+VOLUME ["/var/nomad", "/etc/nomad.d"]
+
+COPY ["./data", "/data"]
+
+RUN ["/bin/bash", "/data/build-nomad.sh"]
+
+# Port info can be found here:
+# https://www.nomadproject.io/docs/install/production/requirements
+
+# TCP           4646            HTTP(S) API
+# TCP           4647            RPC
+# TCP/UDP 	4648 	        Serf
+# TCP/UDP 	20000-32000     Dynamic ports for things
+
+EXPOSE "4646/tcp" \
+       "4647/tcp" \
+       "4648/tcp" \
+       "4648/udp" \
+       "20000-32000/tcp" \
+       "20000-32000/udp"
+
+ENTRYPOINT ["/usr/local/bin/nomad"]
+CMD ["agent", "-config", "/etc/nomad.d"]
---
+++ data/build-nomad.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------------
+# Install Nomad
+# ------------------------------------------------------------------------------
+
+url_prefix="https://releases.hashicorp.com/nomad"
+s6_version="v${S6_VERSION}"
+arch=$(uname -i)
+
+dnf install -y \
+    bash-completion \
+    unzip
+
+# Debian's designation for aarch64 is arm64, so we need to tweak this to fit
+# what s6-overlay uses, otherwise the download fails
+case "${arch}" in
+
+    x86_64)
+        nomad_arch="amd64"
+    ;;
+
+    aarch64)
+        nomad_arch="arm64"
+    ;;
+
+    *)
+        nomad_arch="${arch}"
+    ;;
+esac
+
+
+
+archive="nomad_${NOMAD_VERSION}_linux_${nomad_arch}.zip"
+ckfile="nomad_${NOMAD_VERSION}_SHA256SUMS"
+
+url="${url_prefix}/${NOMAD_VERSION}/${archive}"
+url_ckfile="${url_prefix}/${NOMAD_VERSION}/${ckfile}"
+
+
+echo Downloading $archive from $url...
+curl -L -o "/tmp/${archive}" "${url}"
+curl -L -o "/tmp/${ckfile}" "${url_ckfile}"
+curl -L -o "/tmp/${ckfile}.sig" "${url_ckfile}.sig"
+curl https://keybase.io/hashicorp/key.asc | gpg --import
+
+
+# Check the download signature
+echo Checking tarball signature...
+gpg --verify "/tmp/${ckfile}.sig"
+
+if [[ $? -ne 0 ]]; then
+    (>&2 echo --------------------------------------------------)
+    (>&2 echo --------------------------------------------------)
+    (>&2 echo URGENT: $filename DOES NOT MATCH PGP SIGNATURE!)
+    (>&2 echo IT WAS DOWNLOADED FROM ${url}.)
+    (>&2 echo THIS IS NOT PARTICULARLY GOOD. CHECK YOUR DOWNLOAD.)
+    (>&2 echo BUILD PROCESS TERMINATED. CONTAINER WILL NOT FUNCTION.)
+    (>&2 echo --------------------------------------------------)
+    (>&2 echo --------------------------------------------------)
+    exit 1
+fi
+
+
+echo Installing nomad...
+unzip "/tmp/${archive}" -d /usr/local/bin
+
+# The permissions are probably fine, but let's just be explicit
+chown root:root /usr/local/bin/nomad
+chmod 0755 /usr/local/bin/nomad
+
+mkdir -p /var/nomad/data
+mkdir -p /etc/nomad.d
+
+# Bash completion, because why not?
+nomad -autocomplete-install
+complete -C /usr/local/bin/nomad nomad
+
+# Write simple example server config file
+cat > /etc/nomad.d/nomad.hcl << CONF
+# Full configuration options can be found at https://www.nomadproject.io/docs/configuration
+
+datacenter = "dc01"
+data_dir = "/var/nomad/data"
+bind_addr = "0.0.0.0"
+
+server {
+  enabled = true
+  bootstrap_expect = 1
+}
+CONF
+
+
+# Cleanup
+rm -f "/tmp/${archive}"
+rm -f "/tmp/${ckfile}"
+rm -f "/tmp/${ckfile}.sig"
+
+dnf clean all
+
+echo Nomad installed.
