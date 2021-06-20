bded1fa41d92f1eceb2635f430850bb611558fab
C. Annable <christopher.annable@gmail.com>
2021-06-19 23:05:27 -0400

    Make buildah build script even more generic.

--- build.sh
+++ build.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
+IMAGE="cannable/nomad"
+VERSION_ARG="NOMAD_VERSION"
+ARCHES=(amd64 arm64)
+
 if [[ $# -ne 1 ]]; then
     echo build.sh nomad_version
     exit 1
 fi
 
 version=$1
-arches=(amd64 arm64)
 
-for arch in ${arches[@]}; do
-    buildah bud --arch "$arch" --tag "cannable/nomad:${arch}-${version}" --build-arg "NOMAD_VERSION=${version}" -f ./Dockerfile .
-    buildah push -f v2s2 "cannable/nomad:${arch}-${version}" "docker://cannable/nomad:${arch}-${version}"
+for arch in ${ARCHES[@]}; do
+    buildah bud --arch "$arch" --tag "${IMAGE}:${arch}-${version}" --build-arg "${VERSION_ARG}=${version}" -f ./Dockerfile .
+    buildah push -f v2s2 "${IMAGE}:${arch}-${version}" "docker://${IMAGE}:${arch}-${version}"
 done
 
-buildah manifest create "cannable/nomad:${version}"
+buildah manifest create "${IMAGE}:${version}"
 
-for arch in ${arches[@]}; do
-    buildah manifest add "cannable/nomad:${version}" "docker.io/cannable/nomad:${arch}-${version}"
+for arch in ${ARCHES[@]}; do
+    buildah manifest add "${IMAGE}:${version}" "docker.io/${IMAGE}:${arch}-${version}"
 done
 
-buildah manifest push -f v2s2 "cannable/nomad:${version}" "docker://cannable/nomad:${version}"
+buildah manifest push -f v2s2 "${IMAGE}:${version}" "docker://${IMAGE}:${version}"
 
-buildah manifest rm "cannable/nomad:${version}"
+buildah manifest rm "${IMAGE}:${version}"
